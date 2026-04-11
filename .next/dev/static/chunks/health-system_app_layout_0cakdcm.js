f23308181330dfd9530c2b14959de97c56a633ef
sumitsingh24k <sumitharisingh@gmail.com>
2026-04-11 09:30:16 +0530

    Remove deprecated build and manifest files from the development directory
    - Deleted various JSON and JavaScript files related to build and manifest configurations, including build-manifest.json, fallback-build-manifest.json, and prerender-manifest.json.
    - Cleaned up unused postcss.js and related chunk files to streamline the development environment.
    - Removed runtime.js and its associated map files to eliminate unnecessary dependencies.

--- .next/dev/static/chunks/health-system_app_layout_0cakdcm.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0gns.0b._.css","static/chunks/0kkg_next_dist_01k9d80._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/health-system_app_page_09hz6kq.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0kkg_next_dist_0cr_2p6._.js"],
-    source: "dynamic"
-});
